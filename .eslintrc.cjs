d4a664438d14c6d1be5a9a59469dae51ee4940dd
Adam Langbert <adamhl@pm.me>
2023-08-02 18:27:16 -0500

    cleanup

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "unicorn", "sonarjs"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:unicorn/recommended",
+    "plugin:sonarjs/recommended",
+    "prettier",
+  ],
+  ignorePatterns: [".eslintrc.cjs", "prettier.config.js"],
+}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Plugin } from "obsidian"
+
+export default class ToggleBothSidebars extends Plugin {
+  onload() {
+    this.addCommand({
+      id: "toggle-both-sidebars",
+      name: "Toggle both sidebars",
+      callback: () => {
+        const leftSplit = this.app.workspace.leftSplit
+        const rightSplit = this.app.workspace.rightSplit
+        const isEitherOpen = !(leftSplit.collapsed && rightSplit.collapsed)
+        isEitherOpen ? leftSplit.collapse() : leftSplit.expand()
+        isEitherOpen ? rightSplit.collapse() : rightSplit.expand()
+      },
+    })
+  }
+}
