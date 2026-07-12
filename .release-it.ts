ae6b74ae51aa32f8328af4d0c548df339ee913fc
Adam Langbert <adamhl@pm.me>
2026-07-11 23:50:00 -0500

    ci: simplify release process

--- .release-it.ts
+++ .release-it.ts
@@ -3,7 +3,10 @@ import { releaseItConfig } from "@adamhl8/configs"
 const config = releaseItConfig({
   hooks: {
     // oxlint-disable-next-line no-template-curly-in-string
-    "after:bump": ["bun version-bump.ts ${version}"],
+    "after:bump": ["bun scripts/version-bump.ts ${version}", "bun tsdown"],
+  },
+  github: {
+    assets: ["dist/main.js", "dist/manifest.json"],
   },
 })
 
